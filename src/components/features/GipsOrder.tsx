c71b5dfe17a3bf2886d9a479a247aaa2674fd52d
mitsu0428 <mitsuprograming@gmail.com>
2023-09-25 01:20:41 +0900

    feat: orderまわり

--- src/components/features/GipsOrder.tsx
+++ src/components/features/GipsOrder.tsx
@@ -1,15 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import Image from "next/image";
 import CustomText from "../Tumugi/Text/CustomText/CustomText";
 
 function GipsOrder() {
   return (
     <Wrapper>
-      1. Base Shopで商品を購入する
-      <br />
-      2. ご注文後、1週間以内に商品が届きます
-      <br />
-      3. お届けした商品を飲んで頂きます
+      <IconWrapWithText>
+        <CustomText text="1. Base Shopで商品を購入する" />
+        <Image
+          src="/order-icon/カート.png"
+          width={50}
+          height={50}
+          alt={""}
+        />
+      </IconWrapWithText>
+      <IconWrapWithText>
+        <CustomText text="2. ご注文後、1週間以内に商品が発送されます。" />
+        <Image
+          src="/order-icon/配達.png"
+          width={50}
+          height={50}
+          alt={""}
+        />
+      </IconWrapWithText>
+      <IconWrapWithText>
+        <CustomText text="3. お届けした商品をお受け取りください。" />
+        <Image
+          src="/order-icon/受け取り.png"
+          width={50}
+          height={50}
+          alt={""}
+        />
+      </IconWrapWithText>
     </Wrapper>
   );
 }
@@ -21,5 +44,12 @@ const Wrapper = styled.div`
   border-radius: 8px;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
   overflow-wrap: break-word;
+  padding: 32px;
+`;
+
+const IconWrapWithText = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 16px;
 `;
